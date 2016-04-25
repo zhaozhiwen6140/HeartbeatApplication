79f5c781527ceed0e22a6da0395de50f9ed63b14
zhaozhiwen <1135594680@qq.com>
2016-04-25 20:57:45 +0800

    小小修改

--- app/src/main/java/com/example/zhaozhiwen/heartbeatapplication/FocusDetailActivity.java
+++ app/src/main/java/com/example/zhaozhiwen/heartbeatapplication/FocusDetailActivity.java
@@ -1,7 +1,6 @@
 package com.example.zhaozhiwen.heartbeatapplication;
 
 import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 
 /**
@@ -10,8 +9,8 @@ import android.support.v7.app.AppCompatActivity;
 public class FocusDetailActivity extends AppCompatActivity {
 
     @Override
-    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
-        setContentView(R.layout.fragment_detail);
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.focus_detail);
     }
 }
